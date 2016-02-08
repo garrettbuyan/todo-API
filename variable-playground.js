51b613b0630d9407210357ef6976c631c0db16b7
admin <admin@Rokets-MBP.home>
2016-02-07 20:49:56 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,26 @@
+// var person = {
+//     name: "Garrett",
+//     age: 23
+// }
+//
+// function updatePerson (obj){
+//     // obj = {
+//     //     name: "Garrett",
+//     //     age: 26
+//     // };
+//
+//     obj.age = 26;
+// }
+//
+// updatePerson(person);
+// console.log(person);
+ var grades = [12,89];
+
+ function addGrades(gradesArr){
+     gradesArr.push(67);
+     debugger;
+    //  gradesArr = [28,99,34];
+ }
+
+ addGrades(grades);
+ console.log(grades);
